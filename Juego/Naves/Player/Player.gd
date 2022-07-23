70be40a858705c98bbaba53a95773fd7ac41e539
EmauelCord <emanuelcord@gmail.com>
2022-07-23 08:59:06 -0300

    update

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -14,10 +14,9 @@ onready var canion:Canion = $Canion
 
 #Metodos
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
-	#print(rad2deg(rotation))
-	#print(empuje.rotated(rotation))
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
+
 func _process(delta: float) -> void:
 	player_input()
 
@@ -29,10 +28,17 @@ func player_input() -> void:
 		empuje = Vector2(potencia_motor, 0)
 	elif Input.is_action_pressed("mover_atras"):
 		empuje = Vector2(-potencia_motor, 0)
+
 	#Rotación
 	dir_rotacion = 0
 	if Input.is_action_pressed("rotar_antihorario"):
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
-	
+
+	# Disparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+		
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
